04c41c70c45c7c5fa2e974a9642ed81d91c500e1
ozgemer-git <oz_gemer@outlook.com>
2022-06-15 14:05:37 +0300

    search bar css

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,28 +1,77 @@
+import "./SearchBar.css";
 import { Disclosure } from "@headlessui/react";
 import {
   Search,
   SearchToggle,
   SearchButton,
   FiltersContainer,
+  SearchContainer,
 } from "./SearchBar.styled";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import SearchIcon from "../../Images/search.png";
+import FilterIcon from "../../Images/filter.png";
 
 function SearchBar() {
   return (
     <Disclosure>
-      <Disclosure.Button as={SearchToggle}>&#8595;</Disclosure.Button>
-      <Search></Search>
-      <SearchButton>&#10003;</SearchButton>
+      <SearchContainer>
+        <Disclosure.Button as={SearchToggle}>
+          <img src={FilterIcon} width={30} alt="filter" />
+        </Disclosure.Button>
+        <Search placeholder=" search over 10,000+ recipes..." />
+        <SearchButton>
+          <>
+            <img src={SearchIcon} width={35} alt="search" />
+            SEARCH
+          </>
+        </SearchButton>
+      </SearchContainer>
       <Disclosure.Panel as={FiltersContainer}>
         <FormGroup>
-          <FormControlLabel control={<Checkbox size="small" />} label="spicy" />
           <FormControlLabel
-            control={<Checkbox size="small" />}
+            control={
+              <Checkbox
+                size="small"
+                sx={{
+                  color: "#F59583",
+                  "&.Mui-checked": {
+                    color: "#F8BB86",
+                  },
+                }}
+              />
+            }
+            label="spicy"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                size="small"
+                sx={{
+                  color: "#F59583",
+                  "&.Mui-checked": {
+                    color: "#F8BB86",
+                  },
+                }}
+              />
+            }
             label="vegetarian"
           />
-          <FormControlLabel control={<Checkbox size="small" />} label="vegan" />
+          <FormControlLabel
+            control={
+              <Checkbox
+                size="small"
+                sx={{
+                  color: "#F59583",
+                  "&.Mui-checked": {
+                    color: "#F8BB86",
+                  },
+                }}
+              />
+            }
+            label="vegan"
+          />
         </FormGroup>
       </Disclosure.Panel>
     </Disclosure>
