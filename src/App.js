80a773eaefb18702f452024f17894dba8498c36d
noyflaysher <noyflaysher12@gmail.com>
2022-06-15 14:33:43 +0300

    style css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Body from "./components/body/Body";
 import NavBar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
+import Card from "./components/Card";
 function App() {
   return (
-    <>
+    <Card>
       <NavBar></NavBar>
       <Body></Body>
       <Footer></Footer>
-    </>
+    </Card>
   );
 }
 
