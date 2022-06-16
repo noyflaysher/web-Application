9a454c1066877d6c76474efd8fc1e0de33068556
noyflaysher <noyflaysher12@gmail.com>
2022-06-16 09:21:06 +0300

    recipe component style

--- src/components/body/Recipe.jsx
+++ src/components/body/Recipe.jsx
@@ -1,58 +1,49 @@
 import React from "react";
 import "./Recipe.css";
+import Ingredients from "../Ingredients";
+import { BiTimeFive, BiMinusCircle } from "react-icons/bi";
+import { BsPlusCircle } from "react-icons/bs";
+import { AiOutlineMinusCircle, AiOutlineLike } from "react-icons/ai";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 function Recipe() {
   return (
-    <>
-      <p>start---------------------------------------------</p>
-      <figure>
-        <img />
+    <div className="recipe">
+      <div className="recipe__header">
+        <img className="recipe__image" src="./images/pizza.jpg" />
         <h1>
           <span className="recipe__title">recipe title</span>
         </h1>
-      </figure>
+      </div>
 
       <div class="recipe__details">
         <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--minutes"></span>
+          <span class="recipe__info-data recipe__info-data--minutes">10</span>
           <span class="recipe__info-text">minutes</span>
+          <BiTimeFive />
         </div>
         <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--people"></span>
-          <span class="recipe__info-text">servings</span>
+          <span class="recipe__info-data recipe__info-data--people">5</span>
+          <span class="recipe__info-text">servings </span>
           <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--update-servings">
-              <svg>
-                <use></use>
-              </svg>
+            <button class="btn--tiny btn--update-servings btn--update-servings-plus">
+              <BsPlusCircle />
             </button>
             <button class="btn--tiny btn--update-servings">
-              <svg>
-                <use></use>
-              </svg>
+              <BiMinusCircle />
             </button>
           </div>
         </div>
-        <div class="recipe__user-generated">
-          <svg>
-            <use></use>
-          </svg>
-        </div>
-        <button class="btn--round btn--bookmark ">
-          <svg class="">
-            <use></use>
-          </svg>
+        <button class="btn--tiny btn--like ">
+          <AiOutlineLike />
         </button>
       </div>
+
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
-        <ul class="recipe__ingredient-list"></ul>
+        <ul class="recipe__ingredient-list">
+          <Ingredients />
+        </ul>
       </div>
 
       <div class="recipe__directions">
@@ -62,15 +53,12 @@ function Recipe() {
           <span class="recipe__publisher"></span>. Please check out directions
           at their website.
         </p>
-        <a class="btn--small recipe__btn">
+        <a class="btn--small recipe__btn" href="#" target="_blank">
           <span>Directions</span>
-          <svg class="search__icon">
-            <use></use>
-          </svg>
+          <HiArrowNarrowRight />
         </a>
-        <p>end---------------------------------------------</p>
       </div>
-    </>
+    </div>
   );
 }
 
