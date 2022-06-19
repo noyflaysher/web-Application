3c1747db748fabe286682072998060be426aff6f
noyflaysher <noyflaysher12@gmail.com>
2022-06-19 18:26:48 +0300

    form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Body from "./Components/body/Body";
-import Footer from "./Components/footer/Footer";
-import Card from "./Components/UI/Card";
+import Body from "./Component/body/Body";
+import Footer from "./Component/footer/Footer";
+import Card from "./Component/UI/Card";
+import LogInForm from "./Component/Form/LogInForm";
 function App() {
   return (
     <Card>
       <Body></Body>
       <Footer></Footer>
+
+      <LogInForm />
     </Card>
   );
 }
