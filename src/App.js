0d1c7a4ddd00166db9a8a457c37d7e832ea312a4
noyflaysher <noyflaysher12@gmail.com>
2022-06-20 14:10:47 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Component/footer/Footer";
 import Card from "./Component/UI/Card";
 import LogInForm from "./Component/Form/LogInForm";
 import NavBar from "./Component/NavBar/NavBar";
+
 function App() {
   return (
     <div>
