be5aafdbd40494a077346c9f347f54055929db98
ozgemer-git <oz_gemer@outlook.com>
2022-06-20 15:21:16 +0300

    fixed search-bar button hovers

--- src/Component/SearchBar/SearchBar.styled.jsx
+++ src/Component/SearchBar/SearchBar.styled.jsx
@@ -20,6 +20,8 @@ export const Search = styled.input`
   border: none;
   font-size: 1.2rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  text-align: left;
+  padding: 1rem;
 `;
 
 export const SearchButton = styled.button`
@@ -30,32 +32,37 @@ export const SearchButton = styled.button`
   font-family: inherit;
   font-weight: bold;
   height: 3.5rem;
-  width: 10rem;
+  width: 12rem;
   cursor: pointer;
   background-image: linear-gradient(to right bottom, #e0ffff, #87cefa);
   border-radius: 1.5rem;
   border: none;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   transition: all 0.2s;
-  right: 3rem;
+  right: 2rem;
+  padding-right: 0.5rem;
   position: relative;
   &:hover {
     transform: scale(1.1);
+    font-size: 1.6rem;
+    background: #f4aa8a;
   }
 `;
 
 export const SearchToggle = styled.button`
   height: 3.5rem;
-  width: 4rem;
+  width: 5rem;
   border-radius: 50% 0 0 50%;
   border: none;
   background-image: linear-gradient(to right bottom, #e0ffff, #87cefa);
   cursor: pointer;
-  padding: 0.4rem 0.5rem;
+  padding: 0.3rem 0 0 0.5rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   transition: all 0.2s;
   &:hover {
     transform: scale(1.1);
+    font-size: 1.6rem;
+    background: #f4aa8a;
   }
 `;
 
@@ -64,7 +71,7 @@ export const FiltersContainer = styled.div`
   left: 0;
   top: 4rem;
   background: #f9f5f3;
-  width: 33rem;
+  width: 34rem;
   padding: 0.5rem 1.5rem;
   border-radius: 1.5rem;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
