64d95f1a1c8a8a0f3f7e7be6adfac86bd68144a0
noyflaysher <noyflaysher12@gmail.com>
2022-06-20 14:42:06 +0300

    changes

--- src/Component/Form/SignUp.js
+++ src/Component/Form/SignUp.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
+import { Button } from "@material-ui/core";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
