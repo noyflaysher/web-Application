c5cf9035ad6033ba2e0056b5d566a0a2754e0b5d
noyflaysher <noyflaysher12@gmail.com>
2022-06-21 17:56:22 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Component/footer/Footer";
 import Card from "./Component/UI/Card";
 import LogInForm from "./Component/Form/LogInForm";
 import NavBar from "./Component/NavBar/NavBar";
+import SignUp from "./Component/Form/SignUp";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <NavBar />
         <Body />
         <Footer />
-        <LogInForm />
+        <SignUp />
       </Card>
     </div>
   );
