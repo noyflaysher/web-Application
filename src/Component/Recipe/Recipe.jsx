7967222bce6021e4d5496ae1b52e64f62ac8e1b1
maya koma <mayakoma@gmail.com>
2022-06-22 22:45:47 +0300

    if not connected- login/signup.  else- add  recipe,logout,edit,delete. deletre alarm

--- src/Component/Recipe/Recipe.jsx
+++ src/Component/Recipe/Recipe.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { BiTimeFive } from "react-icons/bi";
 import {
   AiOutlineLike,
   AiFillPlusCircle,
   AiFillMinusCircle,
 } from "react-icons/ai";
+import Modal from "../Map/Modal";
 import Ingredients from "../Ingredient/IngredientsList";
 import Button from "../Button/Button";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
+import { LogContext } from "../../Context/LogContext";
 
 import "./Recipe.css";
 
@@ -52,80 +54,122 @@ const ingredient = [
 //}
 
 function Recipe(props) {
+  const isConnected = useContext(LogContext);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
   const editHandler = {};
-  const deleteHandler = {};
+
+  const showDeteleWarningHandler = () => setShowConfirmModal(true);
+  const cancelDeleteHandler = () => setShowConfirmModal(false);
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log("DELETING...");
+  };
 
   return (
-    <div className="recipe">
-      <div className="recipe__header">
-        <img className="recipe__image" src={props.image} />
-        <h1>
-          <span className="recipe__title">{props.title}</span>
-        </h1>
-      </div>
-      <div className="bottons">
-        <Button onClick={editHandler} className="btn--blue btn--edit">
-          EDIT
-        </Button>
-        <Button onClick={deleteHandler} className="btn--blue btn--del">
-          DELETE
-        </Button>
-      </div>
-      <div className="recipe__details">
-        <div className="recipe__info">
-          <span className="recipe__info-text">Preparation Time: </span>
-          <span className="recipe__info-data">
-            {`${props.time} minutes`}
-            <div>
-              {"\u00A0"} {"\u00A0"}
-            </div>
-            <BiTimeFive />
-          </span>
+    <>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="recipe__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button
+              onClick={cancelDeleteHandler}
+              className="btn--blue btn--cancel"
+            >
+              CANCEL
+            </Button>
+            <Button onClick={confirmDeleteHandler} className="btn--blue">
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this recipe? Please note that it
+          can't be undone thereafter.
+        </p>
+      </Modal>
+      <div className="recipe">
+        <div className="recipe__header">
+          <img className="recipe__image" src={props.image} />
+          <h1>
+            <span className="recipe__title">{props.title}</span>
+          </h1>
         </div>
-        <div className="recipe__info">
-          <span className="recipe__info-text">The recipe is for: </span>
-          <span className="recipe__info-data">
-            {`${props.servings} servings `}
-            <div>
-              {" "}
-              {"\u00A0"} {"\u00A0"}
-            </div>
-            <PeopleAltIcon />
-          </span>
+        <div className="bottons">
+          {isConnected.isLoggedIn && (
+            <>
+              <Button onClick={editHandler} className="btn--blue btn--edit">
+                EDIT
+              </Button>
+              <Button
+                onClick={showDeteleWarningHandler}
+                className="btn--blue btn--del"
+              >
+                DELETE
+              </Button>
+            </>
+          )}
         </div>
-        <div className="recipe__info">
-          <span className="recipe__info-text">publisher:</span>
-          <span className="recipe__info-data">
-            {props.publisher}
-            <div>
-              {" "}
-              {"\u00A0"} {"\u00A0"}
-            </div>
-            <BorderColorIcon />
-          </span>
-        </div>
-      </div>
-      <div className="recipe__flex">
-        <div className="recipe__ingredients">
-          <h2 className="heading--2">Recipe ingredients</h2>
-          <Ingredients data={ingredient} />
-        </div>
-
-        <div className="recipe__directions">
-          <h2 className="heading--2">How to cook it</h2>
-          <div className="recipe__description">
-            <p>{props.description}</p>
+        <div className="recipe__details">
+          <div className="recipe__info">
+            <span className="recipe__info-text">Preparation Time: </span>
+            <span className="recipe__info-data">
+              {`${props.time} minutes`}
+              <div>
+                {"\u00A0"} {"\u00A0"}
+              </div>
+              <BiTimeFive />
+            </span>
+          </div>
+          <div className="recipe__info">
+            <span className="recipe__info-text">The recipe is for: </span>
+            <span className="recipe__info-data">
+              {`${props.servings} servings `}
+              <div>
+                {" "}
+                {"\u00A0"} {"\u00A0"}
+              </div>
+              <PeopleAltIcon />
+            </span>
+          </div>
+          <div className="recipe__info">
+            <span className="recipe__info-text">publisher:</span>
+            <span className="recipe__info-data">
+              {props.publisher}
+              <div>
+                {" "}
+                {"\u00A0"} {"\u00A0"}
+              </div>
+              <BorderColorIcon />
+            </span>
           </div>
         </div>
-      </div>
-      {/* <div>
+        <div className="recipe__flex">
+          <div className="recipe__ingredients">
+            <h2 className="heading--2">Recipe ingredients</h2>
+            <Ingredients data={ingredient} />
+          </div>
+
+          <div className="recipe__directions">
+            <h2 className="heading--2">How to cook it</h2>
+            <div className="recipe__description">
+              <p>{props.description}</p>
+            </div>
+          </div>
+        </div>
+        {/* <div>
         <Button className="btn--blue btn--blue-direction">
           <a href={props.link} target="_blank">
             <span>Directions</span>
           </a>
         </Button>
       </div> */}
-    </div>
+      </div>
+    </>
   );
 }
 
