3f6635fcc6f6a4238b7d80ea84f2690302593c1d
maya koma <mayakoma@gmail.com>
2022-06-22 21:32:30 +0300

    footer style

--- src/Component/footer/Footer.jsx
+++ src/Component/footer/Footer.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
 import { TiLocationOutline } from "react-icons/ti";
-import {Footercontainer,FootercontainerTop,FootercontainerBot,FootercontainerMid} from "./Footer.styled"
+import { WiThermometerExterior } from "react-icons/wi";
+
+import {
+  Footercontainer,
+  FootercontainerTop,
+  FootercontainerBot,
+  FootercontainerMid,
+} from "./Footer.styled";
 import moment from "moment";
 
 function Footer() {
-  const _URL ="http://api.weatherapi.com/v1/current.json?aqi=no";
-  const _KEY="e3c55ad24d104b8c940215558220406";
+  const _URL = "http://api.weatherapi.com/v1/current.json?aqi=no";
+  const _KEY = "e3c55ad24d104b8c940215558220406";
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-  load();
+    load();
   }, []);
 
-  async function load(city="jerusalem"){
-    try{
+  async function load(city = "jerusalem") {
+    try {
       const data = await fetch(`${_URL}&key=${_KEY}&q=${city}`);
       const res = await data.json();
       setWeather(res);
-    } catch(err){}
-    
+    } catch (err) {}
   }
   return (
     <Footercontainer>
-
       <FootercontainerTop>
         <div>
-          <p>Today</p>
           <p>
-            {moment(weather?.location.localtime).format("h:mm a")}
+            {`Date:  ${moment(weather?.location.localtime).format(
+              "MMM Do YY"
+            )},`}
+            {/* <img
+              width="30"
+              height="30"
+              src={weather?.current.condition.icon}
+              alt={`Icon of ${weather?.location.country}`}
+            /> */}
+            {`  ${moment(weather?.location.localtime).format("h:mm a")}`}
           </p>
         </div>
-        <p>
-          {moment(weather?.location.localtime).format("MMM Do YY")}
-        </p>
       </FootercontainerTop>
       <FootercontainerMid>
-        <span>
-          {weather?.current.temp_c} <span>°C</span>
-        </span>
-        <img
-          width="50"
-          height="50"
-          src={weather?.current.condition.icon}
-          alt={`Icon of ${weather?.location.country}`}
-        />
+        <div>
+          <WiThermometerExterior color="#f8c514" />
+          {`Current Temp: ${weather?.current.temp_c} °C`}
+        </div>
       </FootercontainerMid>
       <FootercontainerBot>
-        <TiLocationOutline color='#f8c514'/>
         <p>
+          <TiLocationOutline color="#f8c514" />
+          {"\u00A0"}
           {weather?.location.country}, {weather?.location.name}
         </p>
       </FootercontainerBot>
-
     </Footercontainer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Component/footer/Footer.styled.jsx
+++ src/Component/footer/Footer.styled.jsx
@@ -2,31 +2,36 @@ import styled from "styled-components";
 
 export const Footercontainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  // justify-content: space-between;
   width: 100%;
   margin: auto;
-  padding: 1rem 32rem 1rem 40rem;
+  padding: 1rem 2rem;
   font-size: 1.5rem;
   border-radius: 0 0 1.8rem 1.8rem;
   background-color: white;
 `;
 
 export const FootercontainerTop = styled.div`
-  display: flex;
-  justify-content: space-between;
-  column-gap: 10rem;
+  // display: flex;
+  flex: 1;
+  // justify-content: space-between;
+  // column-gap: 10rem;
   align-items: center;
+  width: 100%;
 `;
 
 export const FootercontainerMid = styled.div`
-  display: flex;
+  // display: flex;
+  flex: 1;
   align-items: center;
-  justify-content: space-between;
+  // justify-content: space-between;
+  width: 100%;
 `;
 
 export const FootercontainerBot = styled.div`
-  display: flex;
-  justify-content: space-between;
+  // display: flex;
+  flex: 1;
+  // justify-content: space-between;
   align-items: center;
-  margin-right: 10rem;
+  width: 100%;
 `;
