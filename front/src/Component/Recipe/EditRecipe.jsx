f959f8a89f5c312d74fb49aa49f4f3d932a26be9
saarmoseri <xgrm406@gmail.com>
2022-07-11 16:29:42 +0300

    delete import

--- front/src/Component/Recipe/EditRecipe.jsx
+++ front/src/Component/Recipe/EditRecipe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
