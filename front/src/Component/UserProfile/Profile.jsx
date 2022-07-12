13b5ce53760510b8ee6744ea93b8f9dd4c3dfa13
saarmoseri <xgrm406@gmail.com>
2022-07-12 20:11:37 +0300

    fixing the profile while user is not log in

--- front/src/Component/UserProfile/Profile.jsx
+++ front/src/Component/UserProfile/Profile.jsx
@@ -6,6 +6,7 @@ import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 import UserUpdate from "./UserUpdate";
 import { UseSession } from "../../Context/Session";
+import LogInForm from "../Form/LogIn";
 import { useHttpClient } from "../hooks/http-hook";
 
 const userBookmarks = [
@@ -23,6 +24,7 @@ const userBookmarks = [
 function Profile(props) {
   const [updateInfo, setUpdateInfo] = React.useState(false);
   const [userRecipes, setUserRecipes] = React.useState([]);
+  const [showLoginForm, setShowLoginForm] = React.useState(false);
   const session = UseSession();
   React.useEffect(() => {
     const requestOption = {
@@ -36,7 +38,7 @@ function Profile(props) {
       }),
     };
 
-    fetch(`http://localhost:3000/recipe/myRecipe`, requestOption) //the db adress and the ver that has the task for the server
+    fetch("http://localhost:3000/recipe/myRecipe", requestOption) //the db adress and the ver that has the task for the server
       .then((response) => (response.ok ? response.json() : { recipe: [] })) //give back the data that just enterd
       .then((data) => {
         setUserRecipes(data.recipe.map((r) => r.title));
@@ -49,36 +51,49 @@ function Profile(props) {
   };
 
   return (
-    <div className="profile-flex">
-      {updateInfo ? <UserUpdate toggle={toggleUpdate} /> : <></>}
-      <img
-        className="profile-img"
-        src="https://previews.123rf.com/images/maxborovkov/maxborovkov1701/maxborovkov170100258/69948331-white-settings-banner-with-silhouettes-of-gears-vector-illustration-.jpg"
-      />
-      <span className="profile-title">PROFILE</span>
-      <div className="profile-button">
-        <UserGroupButton changePass={toggleUpdate} />
-      </div>
-      <span className="profile-subtitle">My Info.</span>
-      <div className="profile-info">
-        <div className="user-info">
-          <span>name: {session.session.name}</span>
-          <span>email: {session.session.email}</span>
+    <>
+      {session.session.userId === null && (
+        <div>
+          <p>You need to login</p>
+          <Button onClick={() => setShowLoginForm(true)}> Login</Button>
+          {showLoginForm && (
+            <LogInForm closeForm={() => setShowLoginForm(false)} />
+          )}
         </div>
-        <span className="profile-subtitle">My Recipes: </span>
-        <div className="user-recipes">
-          <ShowRecipes list={userRecipes} />
+      )}
+      {session.session.userId !== null && (
+        <div className="profile-flex">
+          {updateInfo ? <UserUpdate toggle={toggleUpdate} /> : <></>}
+          <img
+            className="profile-img"
+            src="https://previews.123rf.com/images/maxborovkov/maxborovkov1701/maxborovkov170100258/69948331-white-settings-banner-with-silhouettes-of-gears-vector-illustration-.jpg"
+          />
+          <span className="profile-title">PROFILE</span>
+          <div className="profile-button">
+            <UserGroupButton changePass={toggleUpdate} />
+          </div>
+          <span className="profile-subtitle">My Info.</span>
+          <div className="profile-info">
+            <div className="user-info">
+              <span>name: {session.session.name}</span>
+              <span>email: {session.session.email}</span>
+            </div>
+            <span className="profile-subtitle">My Recipes: </span>
+            <div className="user-recipes">
+              <ShowRecipes list={userRecipes} />
+            </div>
+          </div>
+          <span className="profile-subtitle">Bookmarks</span>
+          <div className="profile-bookmarks">
+            <ShowBookmarks list={userBookmarks} />
+          </div>
+          <span className="profile-subtitle">My Recipes Info.</span>
+          <div className="profile-statistics">
+            <Charts />
+          </div>
         </div>
-      </div>
-      <span className="profile-subtitle">Bookmarks</span>
-      <div className="profile-bookmarks">
-        <ShowBookmarks list={userBookmarks} />
-      </div>
-      <span className="profile-subtitle">My Recipes Info.</span>
-      <div className="profile-statistics">
-        <Charts />
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
