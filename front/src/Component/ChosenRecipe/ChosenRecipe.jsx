804a983bb0b47bd05f771b725014b1c32492569d
saarmoseri <xgrm406@gmail.com>
2022-07-13 16:07:57 +0300

    fix when refresh the recipe stay

--- front/src/Component/ChosenRecipe/ChosenRecipe.jsx
+++ front/src/Component/ChosenRecipe/ChosenRecipe.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Recipe from "../Recipe/Recipe";
-
+import "./ChosenRecipe.css";
 const ChosenRecipe = (props) => {
   const index = useParams().index;
-  const loadedRecipe = props.items[index];
+  const [loadedRecipe, setLoadedRecipe] = React.useState();
+
+  React.useEffect(() => {
+    const setRec = async () => {
+      const tempData = await props.items[index];
+
+      if (tempData) {
+        sessionStorage.setItem("loadedRecipe", JSON.stringify(tempData));
+        setLoadedRecipe(tempData);
+      } else {
+        setLoadedRecipe(JSON.parse(sessionStorage.getItem("loadedRecipe")));
+      }
+    };
+
+    setRec();
+  }, [index]);
+
   return (
     <>
       {loadedRecipe && (
@@ -20,6 +36,13 @@ const ChosenRecipe = (props) => {
           userId={loadedRecipe.userNameId}
           link={loadedRecipe.link}
         />
+      )}
+      {!loadedRecipe && (
+        <div className="chosenRecipe-recipes__container">
+          <header className="chosenRecipe-recipes__header">
+            ops.. its seems that there is no recipe here...
+          </header>
+        </div>
       )}
     </>
   );
